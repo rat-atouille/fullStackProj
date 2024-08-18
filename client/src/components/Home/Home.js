db60d6535090070597736000500e7ba73f43fb6b
rat-atouille <166084207+rat-atouille@users.noreply.github.com>
2024-08-17 23:22:50 -0400

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,20 +1,22 @@
 import './Home.scss';
-import { Link,userNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FaGithub } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div className='homeContainer'>
-      <h1>Cinephile</h1>
-      <h2>Your personal movie tracker</h2>
-      <p>Keep track of movies you have watched and all the movies
-        that you want to watch in the future.
-      </p>
-      <div className='signin'><button><Link to="/Login">GET STARTED</Link></button></div>
-    
-    
-    <div className='footer'>
-      <span className='github'><FaGithub size={20} /></span></div>
+    <div className='home'>
+      <div className='homeContainer'>
+        <h1>Cinephile</h1>
+          <h2>Your personal movie tracker</h2>
+          <p>Keep track of movies you have watched and all the movies
+            that you want to watch in the future.
+          </p>
+          <div className='signin'><button><Link to="/Login">GET STARTED</Link></button></div>
+        </div>
+        <div className='footer'>
+          <span className='github'><a href="https://github.com/rat-atouille/fullStackProj"><FaGithub size={20} /></a></span>
+          <p>© Rataouille</p>
+        </div>    
     </div>
   );
 };
